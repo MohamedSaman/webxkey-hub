fa65d85f36d3cc23cefa1c7a1c883ef2a00d5a10
Saman <mohamedshaman003@gmail.com>
2026-04-29 19:51:01 +0530

    feat: initialize project scaffolding and add server manager UI prototype

---
+++ app/Livewire/Admin/ApplicationsList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class ApplicationsList extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.applications-list');
+    }
+}
